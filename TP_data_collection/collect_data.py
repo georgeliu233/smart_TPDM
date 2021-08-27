6a41765338f36f1117cda705adce3c977d88feca
georgeliu233 <2482976548@qq.com>
2021-08-27 12:37:05 +0800

    sac-traj

--- TP_data_collection/collect_data.py
+++ TP_data_collection/collect_data.py
@@ -8,9 +8,13 @@ import numpy as np
 import pickle
 import argparse
 import matplotlib.pyplot as plt
+from tqdm import trange
 
-def generate_trajs():
+def generate_trajs(each_trajs=1000,steps=50000):
+
     scenarios = '/home/haochen/SMARTS/scenarios/left_turn'
+    save_dir = "/home/haochen/SMARTS_test_TPDM/traj_data/"
+
     smarts = SMARTS(
         agent_interfaces={},
         traffic_sim=SumoTrafficSimulation(headless=True, auto_start=True),
@@ -23,21 +27,88 @@ def generate_trajs():
 
     smarts.reset(next(scenarios_iterator))
 
-    expert_obs = []
-    expert_obs_next = []
-    expert_terminals = []
-    cars_obs = {}
-    cars_obs_next = {}
-    cars_terminals = {}
+    traj_num_cnt = 0
+    # expert_obs = []
+    # expert_obs_next = []
+    # expert_terminals = []
+    # cars_obs = {}
+    # cars_obs_next = {}
+    # cars_terminals = {}
+    obs_dict = {}
+    buf_obs_dict = {}
 
+    exist_dict = {}
     prev_vehicles = set()
-    done_vehicles = set()
-    for i in range(100):
+    curr_vehicles = set()
+
+    traj_num = 0
+    file_cnt = 0
+
+    rst = 0
+    for i in trange(steps):
+
         smarts.step({})
-        current_vehicles = smarts.vehicle_index.social_vehicle_ids()
+
+        #current_vehicles = smarts.vehicle_index.social_vehicle_ids()
         _vehicle_states = [v.state for v in smarts.vehicle_index.vehicles]
-        print(_vehicle_states[0])
-        assert 1==0
+
+        for vehicle_state in _vehicle_states:
+
+            ve_id = vehicle_state.vehicle_id
+
+            if ve_id not in buf_obs_dict:
+                buf_obs_dict[ve_id]=[]
+            curr_vehicles.add(ve_id)
+
+            buf_obs_dict[ve_id].append((vehicle_state,i))
+        
+        if i>rst:
+            #prev_step exist but current step does not exist = traj_done
+            # print(prev_vehicles,curr_vehicles)
+            # assert 1==0
+            done_vehicles = prev_vehicles - curr_vehicles
+            #print(buf_obs_dict)
+            #print(done_vehicles)
+            for v_id in done_vehicles:
+
+                if v_id in obs_dict:
+                    # in case we meet the same vehicle_id
+                    if v_id not in exist_dict:
+                        exist_dict[v_id] = 0
+                    exist_dict[v_id] += 1
+                    obs_dict[v_id+"_re_"+str(exist_dict[v_id])] = buf_obs_dict[v_id]
+                else:    
+                    obs_dict[v_id] = buf_obs_dict[v_id]
+                
+                del buf_obs_dict[v_id]
+                
+                traj_num += 1
+                # if traj_num %20==0:
+                #     print(traj_num)
+                # save traj_files
+                if traj_num >= each_trajs:
+                    obs_dict.update(buf_obs_dict)
+                    print('save file:',file_cnt)
+                    with open(save_dir+"traj_1000_"+str(file_cnt)+'.pkl','wb') as writer:
+                        pickle.dump(obs_dict,writer)
+
+                    file_cnt += 1
+                    traj_num = 0
+                    obs_dict = {}
+                    buf_obs_dict = {}
+                    print('RESET')
+                    smarts.reset(next(scenarios_iterator))
+                    rst = i+1
+
+
+
+        prev_vehicles = curr_vehicles
+        curr_vehicles = set()    
+        #print(_vehicle_states[0])
+
+    with open(save_dir+"_"+str(file_cnt)+'.pkl','wb') as writer:
+        pickle.dump(obs_dict,writer)
+        
 
 def decode_map_xml(path):
     network = SumoRoadNetwork.from_file(path)
@@ -56,6 +127,6 @@ def test_scenario(scenario_root):
     routes = Scenario.discover_routes(scenario_root)
     print(routes)
 if __name__ == "__main__":
-    #generate_trajs()
-    decode_map_xml('/home/haochen/SMARTS/scenarios/intersections/2lane/map.net.xml')
+    generate_trajs()
+    #decode_map_xml('/home/haochen/SMARTS/scenarios/intersections/2lane/map.net.xml')
     #test_scenario('/home/haochen/SMARTS/scenarios/left_turn')
